a390aca8ed277ffee1a76194922970f9395e7117
unknown <natanaelpaes32@gmail.com>
2023-02-07 11:54:05 -0300

    hello world

---
+++ helloworld/src/helloworld/HelloWorld.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("turma 61 é top");
+		System.out.println("tô cum fome");
+	
+		
+	
+	
+	
+
+	}
+
+}
